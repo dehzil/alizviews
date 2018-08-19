00dd233e8a990e40dee2b19f50b826de6615f2f9
AliciaJz <zilaphono@gmail.com>
2018-08-18 19:21:57 -0500

    Title: Side Login
    Details or why the change:
    Now is its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideLoginComponent } from './side-login/side-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideLoginComponent
   ],
   imports: [
     BrowserModule
